51ae1c2dc04924e6a361663fc0bc5e4cc36a2c59
Ardhis Parahita <ardhisparahitaa@gmail.com>
2026-01-04 18:54:34 +0700

    fix: fix swagger

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.set('query parser', 'extended');
+
   app.setGlobalPrefix('/api/v1');
 
   app.useGlobalPipes(
@@ -22,22 +23,24 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors();
+
   const configService = app.get(ConfigService);
   const nodeEnv = configService.get<string>('NODE_ENV') || 'development';
   const port = configService.get<number>('PORT') || 8000;
 
-  if (nodeEnv !== 'production') {
-    const config = new DocumentBuilder()
-      .setTitle('Nest-Blog Api App')
-      .setDescription('Blog API descriptions')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build();
+  const config = new DocumentBuilder()
+    .setTitle('Nest-Blog Api App')
+    .setDescription('Blog API descriptions')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, documentFactory);
-    console.log(`swagger is available at /api`);
-  }
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
+  console.log(`swagger is available at /api`);
 
   await app.listen(port, '0.0.0.0');
 
